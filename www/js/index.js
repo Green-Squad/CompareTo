13dddd42d3bbd2a42f4929ecd77de3e6c2de8c3b
Kyle Dornblaser <kdornblaser@gmail.com>
2014-11-26 20:01:26 -0500

    Populate all dropboxes from database

--- www/js/index.js
+++ www/js/index.js
@@ -15,16 +15,31 @@ app.createTables = function() {
     var db = app.db;
     db.transaction(function(tx) {
         tx.executeSql('CREATE TABLE IF NOT EXISTS metrics (id INTEGER UNIQUE PRIMARY KEY, name TEXT)');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS objects (id INTEGER UNIQUE PRIMARY KEY, name TEXT)');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS metric_object (metric_id INTEGER, object_id INTEGER, value INTEGER, FOREIGN KEY (metric_id) REFERENCES Metrics (id), FOREIGN KEY (object_id) REFERENCES Objects (id))');
     });
 }
+
+
 
 app.seedTables = function() {
     var db = app.db;
     db.transaction(function(tx) {
+        // Seeding metrics
         tx.executeSql('INSERT INTO metrics (id, name) VALUES (1, "Speed")');
         tx.executeSql('INSERT INTO metrics (id, name) VALUES (2, "Weight")');
         tx.executeSql('INSERT INTO metrics (id, name) VALUES (3, "Smell")');
-    });
+
+        // Seeding objects
+        tx.executeSql('INSERT INTO objects (id, name) VALUES (1, "Bird")');
+        tx.executeSql('INSERT INTO objects (id, name) VALUES (2, "Plane")');
+        tx.executeSql('INSERT INTO objects (id, name) VALUES (3, "Superman")');
+
+        // Seeding metric_object
+        tx.executeSql('INSERT INTO metric_object (metric_id, object_id, value) VALUES (1, 1, 10)');      // Metric 1 = Speed, Object 1 = Bird
+        tx.executeSql('INSERT INTO metric_object (metric_id, object_id, value) VALUES (1, 2, 50)');      // Metric 1 = Speed, Object 2 = Plane
+        tx.executeSql('INSERT INTO metric_object (metric_id, object_id, value) VALUES (2, 1, 100)');     // Metric 2 = Weight, Object 1 = Bird
+});
 }
 
 app.onSuccess = function(tx, r) {
@@ -36,20 +51,21 @@ app.onError = function(tx, e) {
     console.log("Error: " + e.message);
 }
 
-app.refresh = function() {
-    var renderMetricsHTML = function (row) {
-        return "<option value='" + row.name + "'>" + row.name + "</option>";
+app.loadMetrics = function() {
+    var addMetrics = function (row) {
+        var option = document.createElement("option");
+        option.text = row.name;
+        option.value = row.id;
+        metricItems.add(option);
     }
 
-    var renderMetrics = function (tx, rs) {
-        var rowOutput = "";
-        var metricItems = document.getElementById("comparison-type");
-        for (var i = 0; i < rs.rows.length; i++) {
-            rowOutput += renderMetricsHTML(rs.rows.item(i));
+    var renderMetrics = function (tx, res) {
+        for (var i = 0; i < res.rows.length; i++) {
+            addMetrics(res.rows.item(i));
         }
-        metricItems.innerHTML = rowOutput;
     }
 
+    var metricItems = document.getElementById("comparison-type");
     var db = app.db;
     db.transaction(function(tx) {
         tx.executeSql("SELECT * FROM metrics", [],
@@ -58,9 +74,43 @@ app.refresh = function() {
     });
 }
 
+app.loadObjects = function() {
+
+    var addObjects = function(row) {
+        var option = document.createElement("option");
+        option.text = row.object_id; // change to object name
+        option.value = row.object_id;
+        document.getElementById("object-1").add(option);
+    }
+
+    var renderObjects = function(tx, res) {
+        for (var i = 0; i < res.rows.length; i++) {
+            console.log(res.rows.item(i));
+            addObjects(res.rows.item(i));
+        }
+        document.getElementById("object-2").innerHTML = document.getElementById("object-1").innerHTML;
+    }
+
+    var metricID = $('#comparison-type').val();
+    document.getElementById("object-1").innerHTML = "<option value='' disabled>Choose an object to compare</option>";
+    $('#object-1-container').show();
+    $('#object-2-container').show();
+
+    var db = app.db;
+    db.transaction(function(tx) {
+        tx.executeSql("SELECT * FROM metric_object WHERE metric_id = ?", [metricID],
+            renderObjects,
+            app.onError);
+    });
+}
+
 function init() {
     app.openDb();
     app.createTables();
     app.seedTables();
-    app.refresh();
+    app.loadMetrics();
 }
+
+$("#comparison-type").change(function() {
+    app.loadObjects();
+});
